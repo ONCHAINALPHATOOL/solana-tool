53d2f6f4e725010d247c1316472cffef9c12e468
ONCHAINALPHATOOL <onchainalpha@gmail.com>
2024-10-14 21:49:44 +0200

    Update solana_tool.py

--- solana_tool.py
+++ solana_tool.py
@@ -8,9 +8,9 @@ def autenticar_google_sheets():
              "https://www.googleapis.com/auth/drive"]
     
     credentials = Credentials.from_service_account_info(
-    st.secrets["GCP_CREDENTIALS"], scopes=scope)
-client = gspread.authorize(credentials)
-
+        st.secrets["GCP_CREDENTIALS"], scopes=scope)
+    
+    client = gspread.authorize(credentials)  # Esta línea debe estar indentada
     return client
 
 # Función para obtener los datos desde Google Sheets
